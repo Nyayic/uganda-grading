98d558297889789d099d6ccd8145f89720f730b7
Nyayic Fanny <chrosbiefannie@gmail.com>
2020-02-25 16:59:32 +0300

    Addeded config file

---
+++ index.js
@@ -0,0 +1,8 @@
+const Marks = require('./models/marks');
+const student = require('./models/student');
+
+const nicholus = new Marks("Math", "Nicholus", 60);
+nicholus.computeScore();
+​
+const daniel = new Marks("Science", "Daniel", 30);
+daniel.computeScore();
---
+++ test/grade.test.js
@@ -0,0 +1,5 @@
+const mark = require('../index')
+test('testing the mark is a number', ()=>{
+  expect(mark.marks).toBe(Number)
+// expect(mark.isValidate()).toBe(true);
+}) 
